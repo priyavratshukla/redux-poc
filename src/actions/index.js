91f57b5ba4cd83204d3900915759a3100b98e256
priyavrat shukla <priyavrat.shukla@publicissapient.com>
2020-07-08 16:35:22 +0530

    action chnages

--- src/actions/index.js
+++ src/actions/index.js
@@ -23,9 +23,3 @@ export const fetchUser = id => async dispatch => {
   dispatch({ type: 'FETCH_USER', payload: response.data });
 };
 
-// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-// const _fetchUser = _.memoize(async (id, dispatch) => {
-//   const response = await jsonPlaceholder.get(`/users/${id}`);
-
-//   dispatch({ type: 'FETCH_USER', payload: response.data });
-// });
